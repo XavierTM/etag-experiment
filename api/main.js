d00e76959d95c4a60ec3e94ef176e3b480f43834
Xavier Mukodi <xavier@xavier.local>
2022-09-20 10:45:25 +0200

    first commit

---
+++ api/main.js
@@ -0,0 +1,46 @@
+console.clear();
+require('dotenv').config();
+
+
+const express = require('express');
+const casual = require('casual')
+
+
+const app = express();
+
+
+function generateData() {
+   return {
+      name: casual.name,
+      age: casual.integer(18, 30)
+   }
+} 
+
+
+app.use(express.json());
+
+app.use(function(req, res, next) {
+   res.set('x-test-header', casual.uuid);
+   next();
+});
+
+let data = generateData();
+
+app.get('/data', function(req, res) {
+   res.send(data);
+});
+
+app.put('/data', (req, res) => {
+   data = generateData();
+   res.send();
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, function() {
+   console.log('Server started at @', PORT);
+})
+
+
+
+
