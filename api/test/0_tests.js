d150abbf5d6d7a91932e43b2ba9cf5296ce97f5c
Xavier Mukodi <xavier@xavier.local>
2022-09-21 10:10:53 +0200

    removed axios tests

--- api/test/0_tests.js
+++ api/test/0_tests.js
@@ -4,13 +4,12 @@ const chaiHttp = require('chai-http');
 const chai = require('chai');
 const assert = chai.assert;
 const server = require('../main');
-const { default: axios } = require('axios');
 
 chai.use(chaiHttp);
 
 suite("chai etag support", () => {
 
-   test("repeating a request to /data should give 304", async () => {
+   test("repeating a request to /data with if-none-match should give 304", async () => {
 
       let res = await chai
          .request(server)
@@ -58,61 +57,11 @@ function behaviourTest(server, suiteName) {
          assert.equal(thirdRequest.status, 304);
          assert.notEqual(thirdRequest.get('x-test-header'), secondRequest.get('x-test-header'), "x-test-headers should be different");
 
-         console.log('text:', thirdRequest.text);
-         console.log('text(first):', firstRequest.text);
-
       });
    });
 
 }
-
-
-function behaviourTestWithAxios(baseURL, suiteName) {
-
-   axios.defaults.baseURL = baseURL;
-
-   suite(suiteName, function() {
-
-      test('Two subsequent GET /data requests should give the same etag', async () => {
-         
-         const firstResponse = await axios.get('/data');
-         const secondResponse = await axios.get('/data');
-
-         assert.notEqual(firstResponse.headers['x-test-header'], secondResponse.headers['x-test-header'], "x-test-headers should be different");
-         assert.equal(firstResponse.headers['etag'], secondResponse.headers['etag'], "Etags should be the same");
-
-
-         const etag = secondResponse.headers['etag'];
-
-         let thirdResponse;
-
-         try {   
-            thirdResponse = await axios.get('/data', {
-               headers: {
-                  'if-none-match': etag
-               }
-            });
-         } catch (err) {
-            thirdResponse = err.response
-            // console.log(thirdResponse);
-         }
-
-         assert.equal(thirdResponse.status, 304);
-         assert.notEqual(secondResponse.headers['x-test-header'], thirdResponse.headers['etag'], "x-test-headers should be different");
-
-
-      });
-   });
-
-}
-
-
 
 
 behaviourTest(process.env.URL, 'Without reverse proxy');
 behaviourTest(process.env.REVERSE_PROXY_URL, 'With reverse proxy');
-
-behaviourTestWithAxios(process.env.URL, 'Without reverse proxy');
-behaviourTestWithAxios(process.env.REVERSE_PROXY_URL, 'With reverse proxy');
-
-// behaviourTestWithAxios(`http://localhost:${process.env.PORT}`, 'Axios against local');
